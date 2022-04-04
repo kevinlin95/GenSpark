90565cc472b01365b31f6f56dc6322ae8657c917
kevinlin95 <contact.kevinlin95@gmail.com>
2022-04-04 10:25:17 -0400

    Update Hangman.java

--- Hangman/src/main/java/Hangman.java
+++ Hangman/src/main/java/Hangman.java
@@ -1,49 +1,108 @@
+import java.util.Scanner;
+import java.util.*;
+// need to add increment to playerguess/missed letters after input
+// add junit testing
+// remove unnecessary methods/fields
+//
 class Hangman
 {
     private String [] computerword;
     private String [] playerguesses;
     private String [] missedletters;
-    private int playerattempts;
+    private int playerattemptsucess = 0;
+    private int playerattemptfailed = 0;
 
-    public Hangman(String[] computerword) { // can set word to guess if wanted
-        this.computerword = computerword;
+    public Hangman (String startingword){
+        computerword = startingword.split("");
+        mainMenu();
     }
 
-    public String[] getComputerword() {
-        return computerword;
-    }
-
-    public void setComputerword(String[] computerword) {
-        this.computerword = computerword;
-    }
-
-    public String[] getPlayerguesses() {
-        return playerguesses;
-    }
-
-    public void setPlayerguesses(String[] playerguesses) {
-        this.playerguesses = playerguesses;
-    }
-
-    public String[] getMissedletters() {
-        return missedletters;
-    }
-
-    public void setMissedletters(String[] missedletters) {
-        this.missedletters = missedletters;
-    }
-
-    public int getPlayerattempts() {
-        return playerattempts;
-    }
-
-    public void setPlayerattempts(int playerattempts) {
-        this.playerattempts = playerattempts;
+    public void addLetter(){
+        try {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Please enter a letter");
+            String temp = sc.next();
+            for (String playerguess : playerguesses) {
+                if (!(temp instanceof String)) {
+                    throw new IllegalArgumentException("Must be a letter");
+                }
+                if (temp.equals(playerguess)) {
+                    System.out.println("You have already guessed that letter." +
+                            "Choose again");
+                }
+                playerguesses[playerattemptsucess] = temp;
+                missedletters[playerattemptfailed] = temp;
+                for (String s : computerword) {
+                    if (Objects.equals(playerguesses[playerattemptsucess], s)) {
+                        playerattemptsucess++;
+                        returnFigureCount(playerattemptfailed);
+                    }
+                    playerattemptfailed++;
+                    returnFigureCount(playerattemptfailed);
+                }
+                returnMissedLetters();
+                returnPlayerGuesses();
+            }
+        }catch(NoSuchElementException e){
+            System.out.println(e);
+        }
     }
 
     public void mainMenu()
     {
         System.out.println("H A N G M A N");
+        do {
+                addLetter();
+        }while(playerattemptfailed < 4);
+        if (playerattemptfailed == 4){
+            System.out.println("Game Over!");
+        }
+    }
+
+    public void returnMissedLetters(){
 
     }
+
+    public void returnPlayerGuesses(){
+
+    }
+
+    public void returnFigureCount(int playerlifeleft){
+        switch (playerlifeleft) {
+            case 1 -> {
+                System.out.println("+---+");
+                System.out.println("O   |");
+                System.out.println("    |");
+                System.out.println("    |");
+                System.out.println("   ===");
+                System.out.println("Attempts remaining: 2");
+            }
+            case 2 -> {
+                System.out.println(" +---+ ");
+                System.out.println(" O   | ");
+                System.out.println("/|\\ | ");
+                System.out.println("     |");
+                System.out.println("    ===");
+                System.out.println("Attempts remaining: 1");
+            }
+            case 3 -> {
+                System.out.println(" +---+ ");
+                System.out.println(" O   | ");
+                System.out.println("/|\\ | ");
+                System.out.println("| |  | ");
+                System.out.println("    ===");
+                System.out.println("Attempts remaining: 0");
+            }
+            default -> {
+                System.out.println("+---+");
+                System.out.println("    |");
+                System.out.println("    |");
+                System.out.println("    |");
+                System.out.println("   ===");
+                System.out.println("Attempts remaining: 3");
+            }
+        }
+    }
 }
+
+
