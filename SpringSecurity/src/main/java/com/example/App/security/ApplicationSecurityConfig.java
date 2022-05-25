73647bf640e5cfcc13cbbe52bdbfbb1cb41efbd7
kevinlin95 <contact.kevinlin95@gmail.com>
2022-05-25 17:27:43 -0400

    5/25

--- SpringSecurity/src/main/java/com/example/App/security/ApplicationSecurityConfig.java
+++ SpringSecurity/src/main/java/com/example/App/security/ApplicationSecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -17,6 +18,7 @@ import static com.example.App.security.ApplicationUserRole.*;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
@@ -31,17 +33,20 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
         UserDetails annaSmithUser = User.builder()
                 .username("annasmith")
                 .password(passwordEncoder.encode("password"))
-                .roles(STUDENT.name()) // Role
+            //    .roles(STUDENT.name()) // ROLE_ABC
+                .authorities(STUDENT.getGrantedAuthorities())
                 .build();
         UserDetails lindaUser = User.builder()
                 .username("linda")
                 .password(passwordEncoder.encode("password123"))
-                .roles(ApplicationUserRole.ADMIN.name()) //ROLE ADMIN
+            //    .roles(ApplicationUserRole.ADMIN.name()) //ROLE ADMIN
+                .authorities(ADMIN.getGrantedAuthorities())
                 .build();
         UserDetails tomUser = User.builder()
                 .username("tom")
                 .password(passwordEncoder.encode("password123"))
-                .roles(ApplicationUserRole.ADMINTRAINEE.name()) //ROLE ADMIN
+            //    .roles(ApplicationUserRole.ADMINTRAINEE.name()) //ROLE ADMIN
+                .authorities(ADMINTRAINEE.getGrantedAuthorities())
                 .build();
         return new InMemoryUserDetailsManager(
                 annaSmithUser,
@@ -53,17 +58,23 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-
+                // Spring sends an CSRF (Cross Site Request Forgery)
+                // as a cookie to the front end server
+                // Client submits form with token -> post/put/delete -> frontend validates
+                // Non Browser client = disable
                 .csrf().disable() // Disables api permission
                 .authorizeRequests()
                 // WhiteListing URL's below
                 .antMatchers("/","index","/css/*","/js*").permitAll()
                 .antMatchers("/students/**").hasRole(STUDENT.name())
                 // Permissions based on ROLE
-                .antMatchers(HttpMethod.DELETE,"/management/api/**").hasAuthority(ApplicationUserPermission.COURSE_WRITE.name())
-                .antMatchers(HttpMethod.POST,"/management/api/**").hasAuthority(ApplicationUserPermission.COURSE_WRITE.name())
-                .antMatchers(HttpMethod.PUT,"/management/api/**").hasAuthority(ApplicationUserPermission.COURSE_WRITE.name())
-                .antMatchers("/management/api/**").hasAnyRole(ADMIN.name(), ADMINTRAINEE.name())
+                // Order of these antMatchers MATTER!!
+                /* These have the same functionality as the @PreAuthorize in Controller Class
+                .antMatchers(HttpMethod.GET,"/management/api/**").hasAnyRole(ADMIN.name(), ADMINTRAINEE.name())
+                .antMatchers(HttpMethod.DELETE,"/management/api/**").hasAuthority(ApplicationUserPermission.COURSE_WRITE.getPermission())
+                .antMatchers(HttpMethod.POST,"/management/api/**").hasAuthority(ApplicationUserPermission.COURSE_WRITE.getPermission())
+                .antMatchers(HttpMethod.PUT,"/management/api/**").hasAuthority(ApplicationUserPermission.COURSE_WRITE.getPermission())
+                */
                 .anyRequest()
                 .authenticated()
                 .and()
--- SpringSecurity/src/main/java/com/example/App/security/ApplicationUserRole.java
+++ SpringSecurity/src/main/java/com/example/App/security/ApplicationUserRole.java
@@ -1,8 +1,11 @@
 package com.example.App.security;
 
 import com.google.common.collect.Sets;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import static com.example.App.security.ApplicationUserPermission.*;
 
@@ -21,6 +24,13 @@ public enum ApplicationUserRole {
         this.permissions = permissions;
     }
 
+    public Set<SimpleGrantedAuthority> getGrantedAuthorities(){
+        Set<SimpleGrantedAuthority> permissions = getPermissions().stream()
+                .map(permission -> new SimpleGrantedAuthority(permission.getPermission()))
+                .collect(Collectors.toSet());
+        permissions.add(new SimpleGrantedAuthority("ROLE_" + this.name()));
+        return permissions;
+    }
 
 
 }
